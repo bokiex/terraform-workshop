4da9c44e56d8e87709fef19970c4e26a0f4d505e
John Koh <kohbokyan@gmail.com>
2024-10-18 20:04:56 +0800

    run workflow

--- section2/terraform/main.tf
+++ section2/terraform/main.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "app_server" {
   subnet_id                   = var.tf_workshop_ex1_subnet_id
   vpc_security_group_ids      = [var.tf_workshop_ex1_vpc_sg_id]
 
+
   tags = {
     Name = "${var.student_id}-Ex1-EC2-Instance"
   }
